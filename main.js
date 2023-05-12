fe09589b06ce1a045659c7bbc5aa2ef9fd2ddc38
Rpg87 <sra.picadillo@gmail.com>
2023-05-12 13:45:18 +0200

    add html, css and start js

---
+++ main.js
@@ -0,0 +1,8 @@
+'use strict'
+const cards = document.querySelectorAll('.memory-card');
+
+function flipCard() {
+    this.classList.toggle('flip');
+}
+
+cards.forEach(card => card.addEventListener('click', flipCard));
